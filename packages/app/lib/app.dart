85b8ce41453e42a149151c1986794341b11f482e
shunsukenoguchi <kesunshuchiguno2525@gmail.com>
2024-11-24 15:40:29 +0900

    書くパッケージでmelos bootstrap

--- packages/app/lib/app.dart
+++ packages/app/lib/app.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
